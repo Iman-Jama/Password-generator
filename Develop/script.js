bfe78d2d7b8a12adb017f64cad2e4b2096518a8d
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-02-16 21:51:16 +0000

    Updated my javascript file, my README and added images to my assets file for my README.

--- Develop/script.js
+++ Develop/script.js
@@ -8,35 +8,36 @@ var generateBtn = document.querySelector("#generate");
   var specialSymbols = "#$%&'()*+,-./:;\"\"<=>?@[]^_`{|}~!";
 
 
-var passwordLength = prompt("Enter a password length, between 8 - 128");
 
-//if length isn't valid, it will stop the script until a valid number is chosen.
-while (!validateNumber(passwordLength)) {
-  
-  passwordLength = prompt("Enter a password length, between 8 - 128");
-};
 
-var specialCharNum = confirm("Do you want numbers in your password?");
-var specialCharUpper = confirm("DO you want uppercase letters?");
-var specialCharLower = confirm("Do you want lowercase letters?");
-var specialCharCharac = confirm("Do you want special characters?");
-
-//This function ensures that the password length is within the required values. Otherwise the user will see an alert which will redirect them to the passwordLength variable.
-function validateNumber(length) {
-  if(length < 8 || length > 128)
-  {
-    alert("PASSWORD MUST BE BETWEEN 8 - 128 CHARACTERS!");
-    return false;
-  } else {
-    return true;
-  }
-}
-
-// I initialized an empty variable called randomPassword. Then i used IF statements to see if the user wanted to use numbers/lowercase/uppercase etc. If the value is true, then this will be added to my variable randomPassword.
 function generatePassword() {
 
-  var randomPassword = "";
+  var passwordLength = prompt("Enter a password length, between 8 - 128");
 
+  //if length isn't valid, it will stop the script until a valid number is chosen.
+  while (!validateNumber(passwordLength)) {
+    
+    passwordLength = prompt("Enter a password length, between 8 - 128");
+  };
+  
+  var specialCharNum = confirm("Do you want numbers in your password?");
+  var specialCharUpper = confirm("Do you want uppercase letters?");
+  var specialCharLower = confirm("Do you want lowercase letters?");
+  var specialCharCharac = confirm("Do you want special characters?");
+  
+  //This function ensures that the password length is within the required values. Otherwise the user will see an alert which will redirect them to the passwordLength variable.
+  function validateNumber(length) {
+    if(length < 8 || length > 128)
+    {
+      alert("PASSWORD MUST BE BETWEEN 8 - 128 CHARACTERS!");
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  var randomPassword = "";
+// I initialized an empty variable called randomPassword. Then i used IF statements to see if the user wanted to use numbers/lowercase/uppercase etc. If the value is true, then this will be added to my variable randomPassword.
   if (specialCharNum == true) {
     randomPassword += numeric;
   }
@@ -55,13 +56,11 @@ function generatePassword() {
   //Loops through password length that has been selected and generates random characters based on selection which was chosen and adds it to the finalPassword each time.
   var finalPassword = ""
   for (let i = 0; i < passwordLength; i++) {
-   //console logged the finalPassword as i wanted to check if it was working correctly. 
-  console.log("generated " + randomPassword[Math.floor(Math.random() * randomPassword.length)]);
-   finalPassword += randomPassword[Math.floor(Math.random() * randomPassword.length)];
+   finalPassword += randomPassword[Math.floor(Math.random()*randomPassword.length)];
   }
   return finalPassword;
  
-}
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -69,7 +68,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 # Password-generator
 
-In this weeks challenge, i was given starter code and asked to complete the code to create a random password generator. The HTML and CSS code were provided and i was not required to change them. Below I will explain the changes I made to the javascript file.
+In this weeks challenge, I was given starter code and asked to complete the code to create a random password generator. Below I will explain how the website works.
 
-## Javascript file
+## **How the password generator website works**
 
-The first step I did was create all the 
+When you access the webpage, you will be presented with an empty box and a red button which says "generate password". Once you click this button, you will be presented with a series of prompts, which give you the option to choose the following:
+1. How long you want your password to be. You must choose a number between 8 - 128 and will be presented with an alert if you do not choose a number within these values.
+2. If you want numbers in your password.
+3. If you want loweercase characters in your password.
+4. If you want Uppercase characters in your password.
+5. If you want special characters in your password. 
 
+Once you have finished going through the prompts and have made your selections, you will be presented with your password.
 
  
-## Below are screenshots of my website:
-![picture](/assets/images/screenshot1.png)
+## **Below are screenshots of my website:**
+![picture](/assets/Screenshot_1.png)
+---
+![picture](/assets/Screenshot_3.png)
+---
 
-  
-![picture](/assets/images/2screenshot.png)
-  
+## **Here is a link to the website:**
+
   
 ### Contact 
   
